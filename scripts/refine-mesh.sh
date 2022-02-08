1db9c04f8e56a17b5866caf2475ff547663a7a71
Matthew Smith <matthew.smith3@crick.ac.uk>
2022-02-08 20:36:19 +0000

    Debugging purposes reduced the number of threads.

--- scripts/refine-mesh.sh
+++ scripts/refine-mesh.sh
@@ -3,4 +3,4 @@ LIB="$SCRIPTS_HOME/lib"
 image=$1
 mesh=$2
 
-java -Xmx128G -cp $LIB/dm3d.jar:$LIB/ij.jar:$LIB/jama.jar $SCRIPTS_HOME/BatchDeformMeshes.java $image $mesh
+java -Xmx48G -cp $LIB/dm3d.jar:$LIB/ij.jar:$LIB/jama.jar $SCRIPTS_HOME/BatchDeformMeshes.java $image $mesh
