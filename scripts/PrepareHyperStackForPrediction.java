69a1a28aa027313a8df9f8e19fab8fad8601612f
Matthew Smith <mbs@here.now>
2022-02-26 20:33:45 +0100

    Adjusts the contrast using the brightest slice from the channel.

--- scripts/PrepareHyperStackForPrediction.java
+++ scripts/PrepareHyperStackForPrediction.java
@@ -1,3 +1,6 @@
+import ij.ImageJ;
+import ij.process.ImageConverter;
+import ij.process.ImageStatistics;
 import loci.common.DebugTools;
 import loci.formats.FormatException;
 import loci.plugins.BF;
@@ -15,6 +18,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrepareHyperStackForPrediction {
 
@@ -39,14 +44,36 @@ public class PrepareHyperStackForPrediction {
         ImageStack in = plus.getImageStack();
         Files.createDirectories(outFolder);
         for(int i = 0; i<frames; i++){
+            List<ImageStack> cStacks = new ArrayList<>();
+            for(int ci = 0; ci<selectedChannels.length; ci++){
+                ImageStack stack = new ImageStack(plus.getWidth(), plus.getHeight());
+                int brightestSlice = slices/2;
+                double max = 0;
+                for(int z = 0; z < slices; z++){
+                    int N = z*channels + i * channels * slices + selectedChannels[ci];
+                    ImageProcessor processor = in.getProcessor( N );
+                    stack.addSlice(in.getSliceLabel(N), processor);
+                    ImageStatistics is = processor.getStatistics();
+                    if(is.max > max){
+                        max = is.max;
+                        brightestSlice = z;
+                    }
+                }
+                ImagePlus tmp = plus.createImagePlus();
+                tmp.setStack(stack, 1, slices, 1);
+                tmp.setSlice(brightestSlice);
+                tmp.resetDisplayRange();
+                ImageConverter converter = new ImageConverter(tmp);
+                ImageConverter.setDoScaling(true);
+                converter.convertToGray8();
+                cStacks.add(tmp.getImageStack());
+            }
             ImageStack out = new ImageStack(in.getWidth(), in.getHeight());
             for(int z = 0; z < slices; z++){
-                for(int c: selectedChannels){
-                    int N = z*channels + i * channels * slices + c;
-                    ImageProcessor processor = in.getProcessor( N );
-
-                    out.addSlice( in.getSliceLabel( N ), processor.convertToByte(true));
-
+                for(int ci = 0; ci<selectedChannels.length; ci++){
+                    ImageStack stack = cStacks.get(ci);
+                    ImageProcessor processor = stack.getProcessor( z + 1 );
+                    out.addSlice( stack.getSliceLabel(z+1), processor);
                 }
             }
             Path outpath = outFolder.resolve( src.getFileName().toString().replace(".tif", String.format("-2b-%04d.tif", i) ) );
@@ -55,7 +82,6 @@ public class PrepareHyperStackForPrediction {
             dup.setStack(out, selectedChannels.length, slices, 1);
             dup.setLut(LUT.createLutFromColor(Color.WHITE));
             IJ.save(dup, outpath.toString());
-
         }
 
 
